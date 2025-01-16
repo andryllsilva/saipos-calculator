319e1c358a741a2ecf1c2eb6ee3844533c17e50e
Andryll Silva <andryll.silva@saipos.com>
2025-01-16 20:51:35 -0300

    first commit

---
+++ Scripts/controller/CalcController.js
@@ -0,0 +1,138 @@
+class CalcController {
+  constructor() {
+    this._operation = []
+    this._locale = 'pt-BR'
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._currentDate;
+    this.initialize();
+    this.initButtonsEvents();
+
+  }
+
+  initialize() {
+    this.setDisplayDateTime()
+    let interval = setInterval(() => {
+      this.setDisplayDateTime()
+    }, 1000);
+  }
+
+  addEventListenerAll(element, events, fn){
+    events.split(' ').forEach(event => {
+      element.addEventListener(event, fn, false);
+    })
+  }
+
+  setError(){
+    this.displayCalc = "error 404"
+  }
+
+  clearAll(){
+    this._operation = [];
+  }
+
+  clearEntry(){
+    this._operation.pop();
+  }
+
+  addOperation(value){
+    this._operation.push(value);
+    console.log(this._operation)
+  }
+
+  execBtn(value){
+    
+    switch(value){
+      case 'ac':
+        this.clearAll();
+        break;
+      case 'ce':
+        this.clearEntry();
+        break;
+      case 'soma':
+        
+      break;
+      case 'subtracao':
+        
+      break;
+      case 'multiplicacao':
+        
+      break;
+      case 'porcento':
+        
+      break;
+      case 'igual':
+
+      default:
+        this.setError()
+      break;
+
+
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.addOperation(parseInt(value));
+        break;
+      case '0':
+    }
+  }
+
+  initButtonsEvents(){
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g" );
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, 'click drag', e =>{
+        let textBtn = btn.className.baseVal.replace("btn-", "");
+
+        this.execBtn(textBtn);
+      })
+
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+        btn.style.cursor = "pointer"
+      })
+    });
+  }
+
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    this._timeEl.innerHTML = value
+  }
+
+  set displayDate(value) {
+    this._dateEl.innerHTML = value
+  }
+
+  get displayCalc() {
+    return this._displayCalcEl.innerHTML;;
+  }
+
+  set displayCalc(valor) {
+    this._displayCalcEl.innerHTML = valor;
+  }
+
+  get currentDate() {
+    return new Date();
+  }
+
+
+
+}
